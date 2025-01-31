448f83cd5968edb5eda4e66f00d1d531c4ae68c9
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2025-01-31 16:11:19 -0300

    feat: add auth to /dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,16 @@
+import { authOptions } from "@/lib/auth";
 import { Container } from "../../components/container";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 
-export default function Dashboard() {
+export default async function Dashboard() {
+    const session = await getServerSession(authOptions);
+
+    if(!session || !session.user) {
+        redirect("/");
+    }
+
     return (
         <Container>
             <h1>Página Dashboard</h1>
