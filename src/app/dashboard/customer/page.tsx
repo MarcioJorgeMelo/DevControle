918d68c41ea1c0ec72455b13f7659e373929dd06
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2025-02-01 15:56:37 -0300

    feat: customer page

--- src/app/dashboard/customer/page.tsx
+++ src/app/dashboard/customer/page.tsx
@@ -2,6 +2,8 @@ import { Container } from "@/components/container";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import Link from "next/link";
+import { Card } from "./components/card";
 
 
 export default async function Customer() {
@@ -13,10 +15,20 @@ export default async function Customer() {
 
     return (
         <Container>
-            <main>
-                <div>
-                    <h1>Meus clientes</h1>
+            <main className="mt-9 mb-2">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-3xl font-bold">Meus clientes</h1>
+
+                    <Link href="/dashboard/customer/new" className="bg-blue-500 text-white px-4 py-1 rounded">
+                        Novo cliente
+                    </Link>
                 </div>
+
+                <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-2">
+                    <Card />
+                    <Card />
+                    <Card />
+                </section>
             </main>
         </Container>
     )
