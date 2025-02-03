4a285916ffd2ca1818046f35ac10ddf2fdb98556
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2025-02-03 20:29:54 -0300

    feat: using route api to delete customer on card component

--- src/app/dashboard/customer/components/card/index.tsx
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -1,7 +1,28 @@
+"use client"
+import { api } from "@/lib/api";
 import { CustomerProps } from "@/utils/customer.type";
+import { useRouter } from "next/navigation";
 
 
 export function Card({ customer }: { customer: CustomerProps }) {
+    const router = useRouter();
+
+    async function handleDeleteCustomer() {
+        try {
+
+            const response = await api.delete("/api/customer", {
+                params: {
+                    id: customer.id
+                }
+            });
+
+            router.refresh();
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <article className="flex flex-col bg-gray-100 border-2 p-2 rounded-lg gap-2 hover:scale-105 duration-300">
             <h2><a className="font-bold">Nome:</a> {customer.name}</h2>
@@ -10,7 +31,10 @@ export function Card({ customer }: { customer: CustomerProps }) {
 
             <p><a className="font-bold">Telefone:</a> {customer.phone}</p>
 
-            <button className="bg-red-500 px-4 rounded text-white mt-2 self-start">
+            <button
+                className="bg-red-500 px-4 rounded text-white mt-2 self-start"
+                onClick={handleDeleteCustomer}
+            >
                 Deletar
             </button>
         </article>
