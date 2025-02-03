20dabd485eee0ec46fbd979264c5c13e873689f7
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2025-02-03 20:28:39 -0300

    feat: create route api to delete customer

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -3,6 +3,44 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import prismaClient from '@/lib/prisma';
 
+export async function DELETE(request: Request) {
+    const session = await getServerSession(authOptions);
+
+    if(!session || !session.user) {
+        return NextResponse.json({ error: "Not authorized" }, { status: 401 });
+    }
+
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("id");
+
+    if(!userId) {
+        return NextResponse.json({ error: "Failed delete customer" }, { status: 400 });
+    }
+
+    const findTickets = await prismaClient.ticket.findFirst({
+        where: {
+            customerId: userId
+        }
+    })
+
+    if(findTickets) {
+        return NextResponse.json({ error: "Failed delete customer because customer have pending orders" }, { status: 400 });
+    }
+
+    try {
+        
+        await prismaClient.customer.delete({
+            where: {
+                id: userId as string
+            }
+        })
+
+        return NextResponse.json({ message: "Cliente deletado com sucesso!" });
+
+    } catch (error) {
+        return NextResponse.json({ error: "Failed delete customer" }, { status: 400 });
+    }
+}
 
 export async function POST(request: Request) {
     const session = await getServerSession(authOptions);
