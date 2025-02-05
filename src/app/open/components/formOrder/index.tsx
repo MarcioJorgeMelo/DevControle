b64a95de4be99f913a4865980987705c1d2d6145
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2025-02-05 18:41:00 -0300

    feat: add call new route api to register order

--- src/app/open/components/formOrder/index.tsx
+++ src/app/open/components/formOrder/index.tsx
@@ -3,6 +3,8 @@ import { Input } from "@/components/input";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { api } from "@/lib/api";
+import { CustomerDataInfo } from "../../page";
 
 const schema = z.object({
     name: z.string().min(1, "O nome do chamado é obrigatório."),
@@ -11,13 +13,28 @@ const schema = z.object({
 
 type FormData = z.infer<typeof schema>
 
-export function FormOrder() {
+interface FormDataProps {
+    customer: CustomerDataInfo;
+}
+
+export function FormOrder({ customer }: FormDataProps) {
     const { register, handleSubmit, setValue, formState: { errors } } = useForm<FormData>({
         resolver: zodResolver(schema)
     });
 
+    async function handleRegisterOrder(data: FormData) {
+        const response = await api.post("/api/order", {
+            name: data.name,
+            description: data.description,
+            customerId: customer.id
+        })
+    }
+
     return (
-        <form className="bg-slate-200 mt-6 px-4 py-6 rounded border-2">
+        <form
+            className="bg-slate-200 mt-6 px-4 py-6 rounded border-2"
+            onSubmit={handleSubmit(handleRegisterOrder)}
+        >
             <label className="mb-1 font-medium text-lg">
                 Nome do chamado
             </label>
@@ -35,12 +52,12 @@ export function FormOrder() {
             </label>
 
             <textarea
-                className="w-full border-2 rounded-md h-24 resize-none mb-2 px-2"
+                className="w-full border-2 rounded-md h-24 resize-none px-2"
                 placeholder="Descreva seu problema"
                 id="description"
                 {...register("description")}
             />
-            {errors.description?.message && <p className="text-red-500 my-1">{errors.description.message}</p>}
+            {errors.description?.message && <p className="text-red-500 mb-1 mt-4">{errors.description.message}</p>}
 
             <button 
                 type="submit" 
