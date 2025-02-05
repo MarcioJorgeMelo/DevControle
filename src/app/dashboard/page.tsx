9a17b34619efd2f2328b93b0d616207e9dd81507
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2025-02-05 18:57:09 -0300

    feat: add button refresh component on page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import Link from "next/link";
 import { TicketItem } from "./components/ticket";
 import prismaClient from "@/lib/prisma";
+import { ButtonRefresh } from "./components/buttonRefresh";
 
 
 export default async function Dashboard() {
@@ -34,9 +35,14 @@ export default async function Dashboard() {
             <main className="mt-9 mb-2">
                 <div className="flex items-center justify-between">
                     <h1 className="text-3xl font-bold">Chamados</h1>
-                    <Link href="/dashboard/new" className="bg-blue-500 px-4 py-1 rounded text-white" >
-                        Abrir chamado
-                    </Link>
+
+                    <div className="flex items-center justify-center gap-3">
+                        <ButtonRefresh />
+
+                        <Link href="/dashboard/new" className="bg-blue-500 px-4 py-1 rounded text-white" >
+                            Abrir chamado
+                        </Link>
+                    </div>
                 </div>
 
                 <table className="min-w-full my-2">
